c785e5f89ba96938e8940e14c33fa2d92741aadc
Gaurav Khatri <gaukhatr@cisco.com>
2022-12-05 01:11:39 -0800

    Add license header

--- pkg/qr/qr_test.go
+++ pkg/qr/qr_test.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2022 Gaurav Khatri. All rights reserved.
+//
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
 package qr
 
 import (
